57bfa94310731510ec5848a49965295bfd410dae
Adi Gunawan Lase <adiglase6@gmail.com>
2023-06-14 23:50:38 +0700

    fix error app cannot load on prod environment

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask.cli import FlaskGroup
-from project.app import app
+from project.app import app_ins
 
-cli = FlaskGroup(app)
+cli = FlaskGroup(app_ins)
 
 if __name__ == "__main__":
     cli()
